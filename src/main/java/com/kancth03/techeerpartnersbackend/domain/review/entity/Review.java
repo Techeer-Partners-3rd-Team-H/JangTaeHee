2a24eb62fca953ff2fa9ad534220052159e1b6a8
SNOW <kancth03@gmail.com>
2023-10-19 07:04:12 +0900

    feat: Review 엔티티 생성 및 Restaurant 엔티티와 1:N 연결

---
+++ src/main/java/com/kancth03/techeerpartnersbackend/domain/review/entity/Review.java
@@ -0,0 +1,40 @@
+package com.kancth03.techeerpartnersbackend.domain.review.entity;
+import com.kancth03.techeerpartnersbackend.domain.restaurant.entity.Restaurant;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table
+@Getter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 300)
+    private String title;
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant_id;
+
+    private Review(String title, String content, Restaurant restaurant_id) {
+        this.title = title;
+        this.content = content;
+        this.restaurant_id = restaurant_id;
+    }
+
+    public static Review of(String title, String content, Restaurant restaurant_id) {
+        return new Review(title, content, restaurant_id);
+    }
+}
