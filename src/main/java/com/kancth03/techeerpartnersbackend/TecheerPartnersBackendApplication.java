472c4efd6c7ea7a86ed3629818ac253d5cc76013
TaeHee00 <kancth03@gmail.com>
2023-10-19 06:06:06 +0900

    Spring boot init

---
+++ src/main/java/com/kancth03/techeerpartnersbackend/TecheerPartnersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kancth03.techeerpartnersbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerPartnersBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecheerPartnersBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kancth03/techeerpartnersbackend/TecheerPartnersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kancth03.techeerpartnersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerPartnersBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
