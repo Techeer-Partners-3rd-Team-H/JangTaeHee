f0a5deedf4d110105af29188f8fb2be63464f50d
SNOW <kancth03@gmail.com>
2023-10-19 06:53:44 +0900

    feat: 레스토랑데이터 추가시 @CreateDate를 사용하여 생성일시 자동 삽입을 위해 @EnableJpaAuditing 추가

--- src/main/java/com/kancth03/techeerpartnersbackend/TecheerPartnersBackendApplication.java
+++ src/main/java/com/kancth03/techeerpartnersbackend/TecheerPartnersBackendApplication.java
@@ -2,7 +2,9 @@ package com.kancth03.techeerpartnersbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TecheerPartnersBackendApplication {
 
