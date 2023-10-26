dd63d0b521b981139bbd0c86e7c245dabf17b9c4
SNOW <kancth03@gmail.com>
2023-10-26 23:43:52 +0900

    fix: review dto 사용하지않는 import 정리

--- src/main/java/com/kancth03/techeerpartnersbackend/domain/review/dto/AddReviewRequest.java
+++ src/main/java/com/kancth03/techeerpartnersbackend/domain/review/dto/AddReviewRequest.java
@@ -1,7 +1,5 @@
 package com.kancth03.techeerpartnersbackend.domain.review.dto;
 
-import com.kancth03.techeerpartnersbackend.domain.review.entity.Review;
-
 public record AddReviewRequest(
         Long restaurantId,
         String title,
