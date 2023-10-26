53cb3282e214462d2a96d652a8836b859dded88d
TaeHee00 <kancth03@gmail.com>
2023-10-26 19:15:47 +0900

    feat: 레스토랑 카테고리 변경 비즈니스 로직 구현

--- src/main/java/com/kancth03/techeerpartnersbackend/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/kancth03/techeerpartnersbackend/domain/restaurant/service/RestaurantService.java
@@ -1,8 +1,11 @@
 package com.kancth03.techeerpartnersbackend.domain.restaurant.service;
 
 import com.kancth03.techeerpartnersbackend.domain.restaurant.dto.AddRestaurantRequest;
-import com.kancth03.techeerpartnersbackend.domain.restaurant.entity.Restaurant;
 import com.kancth03.techeerpartnersbackend.domain.restaurant.dto.AddRestaurantResponse;
+import com.kancth03.techeerpartnersbackend.domain.restaurant.dto.ModifyCategoryRequest;
+import com.kancth03.techeerpartnersbackend.domain.restaurant.dto.ModifyCategoryResponse;
+import com.kancth03.techeerpartnersbackend.domain.restaurant.entity.Restaurant;
+import com.kancth03.techeerpartnersbackend.domain.restaurant.entity.RestaurantCategory;
 import com.kancth03.techeerpartnersbackend.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,11 +25,33 @@ public class RestaurantService {
         return AddRestaurantResponse.entityToDto(savedRestaurant);
     }
 
+    public ModifyCategoryResponse modifyCategory(ModifyCategoryRequest request) {
+        // 레스토랑 id 존재 확인
+        restaurantValidate(request.id());
+
+        Restaurant restaurant = restaurantRepository.findById(request.id()).orElseThrow();
+        RestaurantCategory oldCategory = restaurant.getCategory();
+
+        restaurant.setCategory(request.category());
+        // 쿼리 확인을 위해 바로 save
+        restaurantRepository.save(restaurant);
+
+        return new ModifyCategoryResponse(restaurant.getName(), oldCategory, restaurant.getCategory());
+    }
+
     private void duplicateValidate(String restaurantName) {
         Optional<Restaurant> findRestaurant = restaurantRepository.findByName(restaurantName);
 
         if (findRestaurant.isPresent()) {
             throw new IllegalArgumentException("레스토랑명 중복");
+        }
+    }
+
+    private void restaurantValidate(Long restaurantId) {
+        Optional<Restaurant> findRestaurant = restaurantRepository.findById(restaurantId);
+
+        if (findRestaurant.isEmpty()) {
+            throw new IllegalArgumentException("존재하지 않는 레스토랑");
         }
     }
 }
