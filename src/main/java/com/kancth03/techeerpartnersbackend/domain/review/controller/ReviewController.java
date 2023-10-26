75a6726efa356be7095d92dbae64392a2fab55de
SNOW <kancth03@gmail.com>
2023-10-26 23:34:20 +0900

    feat: Review Controller 리뷰 삭제기능 구현

--- src/main/java/com/kancth03/techeerpartnersbackend/domain/review/controller/ReviewController.java
+++ src/main/java/com/kancth03/techeerpartnersbackend/domain/review/controller/ReviewController.java
@@ -4,10 +4,7 @@ import com.kancth03.techeerpartnersbackend.domain.review.dto.AddReviewRequest;
 import com.kancth03.techeerpartnersbackend.domain.review.dto.ReviewResponse;
 import com.kancth03.techeerpartnersbackend.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +16,10 @@ public class ReviewController {
     @PostMapping
     public ReviewResponse addReview(@RequestBody AddReviewRequest request) {
         return reviewService.addReview(request);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public void deleteReview(@PathVariable Long reviewId) {
+        reviewService.deleteReview(reviewId);
     }
 }
