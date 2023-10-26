4778f52edf8ecc79ff8e6de9aa093527ebedbbc7
TaeHee00 <kancth03@gmail.com>
2023-10-26 19:00:44 +0900

    fix: exception handler detail message부분 수정

--- src/main/java/com/kancth03/techeerpartnersbackend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kancth03/techeerpartnersbackend/global/exception/GlobalExceptionHandler.java
@@ -14,11 +14,12 @@ public class GlobalExceptionHandler {
     // Controller하나 만들어서 분리해주는것도 괜찮을꺼같기도한데..
     @ExceptionHandler(IllegalArgumentException.class)
     protected ResponseEntity<ErrorResponse> illegalArgumentException(IllegalArgumentException e) {
+
         final ErrorResponse errorResponse = ErrorResponse.builder(
                 new IllegalArgumentException(),
                 HttpStatus.BAD_REQUEST,
                 e.getMessage())
-                .detailMessageArguments("이미 사용중인 레스토랑명입니다.")
+                .detailMessageArguments(e.getMessage())
                 .build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
